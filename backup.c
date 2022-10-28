b5dac01b022cda9d3affb6c76053715e36f11cb9
Artem Maslov <artem.m198@gmail.com>
2022-10-28 16:46:03 +0300

    gzip added

--- backup.c
+++ backup.c
@@ -26,6 +26,16 @@ static void BackupTreeDump(FileInfo *dirInfo, size_t level);
 
 static bool CheckNeedCopy(FileInfo* srcFile, FileInfo* destFile);
 
+static bool FileMaskGzip(FileInfo* fileInfo);
+
+static bool FileCheckGzip(const FileInfo* const fileInfo);
+
+static bool DirectoryAddPath(FileInfo* dirInfo, const char* const path, const char* const fileName);
+
+static bool DirectoryComparatorDest2(const void* const elem1, const void* const elem2);
+
+static bool DirectoryComparatorDest1(const void* const elem1, const void* const elem2);
+
 //***///***///---\\\***\\\***\\\___///***___***\\\___///***///***///---\\\***\\\***//
 //***///***///---\\\***\\\***\\\___///***___***\\\___///***///***///---\\\***\\\***//
 
@@ -91,10 +101,19 @@ void DirectoryBackup(const char *const src, const char *const dest)
     }
     else
     {
-        printf("Копирование \"%s\" в \"%s\".\n", srcDir.FilePath, destDir.FilePath);
+        printf("Рекурсивное копирование \"%s\" в \"%s\".\n", srcDir.FilePath, destDir.FilePath);
         if (!BashCp(srcDir.FilePath, destDir.FilePath))
         {
             fprintf(stderr, "Ошибка копирования файлов.\n");
+            FileInfoDestructor(&srcDir);
+            FileInfoDestructor(&destDir);
+            return;
+        }
+
+        printf("Рекурсивное сжатие \"%s\".\n", destDir.FilePath);
+        if (!BashGzip(destDir.FilePath))
+        {
+            fprintf(stderr, "Ошибка сжатия файлов.\n");
             FileInfoDestructor(&srcDir);
             FileInfoDestructor(&destDir);
             return;
@@ -108,15 +127,35 @@ void DirectoryBackup(const char *const src, const char *const dest)
 //***///***///---\\\***\\\***\\\___///***___***\\\___///***///***///---\\\***\\\***//
 //***///***///---\\\***\\\***\\\___///***___***\\\___///***///***///---\\\***\\\***//
 
-static bool DirectoryComparator(const void* const elem1, const void* const elem2)
+static bool DirectoryComparatorDest2(const void* const elem1, const void* const elem2)
 {
     assert(elem1);
     assert(elem2);
 
-    FileInfo* dir1 = (FileInfo*)elem1;
-    FileInfo* dir2 = (FileInfo*)elem2;
+    FileInfo* srcDir = (FileInfo*)elem1;
+    FileInfo* destDir = (FileInfo*)elem2;
 
-    return strncmp(dir1->FileName, dir2->FileName, FILE_NAME_SIZE) == 0;
+    if (srcDir->FileType != destDir->FileType)
+        return false;
+
+    // Пропускаем удаленные файлы.
+    if (destDir->FileName[0] == '\0')
+        return false;
+
+    // У файла игнорируем расширение ".gz"
+    // У директории нет
+    if (srcDir->FileType == FILE_TYPE_DIR)
+        return strncmp(srcDir->FileName, destDir->FileName, FILE_NAME_SIZE) == 0;
+    else
+    {
+        size_t len = strlen(destDir->FileName) - 3;
+        return strncmp(srcDir->FileName, destDir->FileName, len) == 0;
+    }
+}
+
+static bool DirectoryComparatorDest1(const void* const elem1, const void* const elem2)
+{
+    return DirectoryComparatorDest2(elem2, elem1);
 }
 
 static bool BackupClear(FileInfo* srcDir, FileInfo* destDir)
@@ -128,9 +167,20 @@ static bool BackupClear(FileInfo* srcDir, FileInfo* destDir)
     for (size_t destIndex = 0; destIndex < destFilesCount; destIndex++)
     {
         FileInfo *destFile = (FileInfo *)ExtArrayGetElemAt(&destDir->Children, destIndex);
-        size_t srcIndex = ExtArrayFindElem(&srcDir->Children, destFile, DirectoryComparator);
+        size_t srcIndex = ExtArrayFindElem(&srcDir->Children, destFile, DirectoryComparatorDest2);
 
-        if (srcIndex == -1)
+        if (destFile->FileType != FILE_TYPE_DIR && FileCheckGzip(destFile) == false)
+        {
+            printf("Удаление не сжатого файла \"%s\".\n", destFile->FilePath);
+            if (!BashRm(destFile->FilePath))
+            {
+                fprintf(stderr, "Ошибка копирования файлов.\n");
+                return false;
+            }
+            // Файл не сжат, его нужно убрать из дерева файлов. Иначе он может быть перепутан сжатым (при сравнении файлов расширение .gz игнорируется).
+            destFile->FileName[0] = '\0';
+        }
+        else if (srcIndex == -1)
         {
             printf("Удаление файла \"%s\".\n", destFile->FilePath);
             if (!BashRm(destFile->FilePath))
@@ -138,6 +188,8 @@ static bool BackupClear(FileInfo* srcDir, FileInfo* destDir)
                 fprintf(stderr, "Ошибка копирования файлов.\n");
                 return false;
             }
+            // Этот файл был удалён в исходной директории. Отбрасываем его для более быстрого сравнения.
+            destFile->FileName[0] = '\0';
         }
         else
         {
@@ -163,6 +215,57 @@ static bool CheckNeedCopy(FileInfo* srcFile, FileInfo* destFile)
     return srcFile->Time > destFile->Time;
 }
 
+static bool FileCheckGzip(const FileInfo* const fileInfo)
+{
+    assert(fileInfo);
+
+    if (fileInfo->FilePathSize < 3)
+        return false;
+
+    char* ptr = fileInfo->FilePath;
+    size_t st = fileInfo->FilePathSize - 3;
+    if (ptr[st] == '.' && ptr[st + 1] == 'g' && ptr[st + 2] == 'z')
+        return true;
+
+    return false;
+}
+
+static bool FileMaskGzip(FileInfo* fileInfo)
+{
+    assert(fileInfo);
+
+    if (fileInfo->FilePathSize < 3)
+        return false;
+
+    char* ptr = fileInfo->FilePath;
+    size_t st = fileInfo->FilePathSize - 3;
+    if (ptr[st] == '.' && ptr[st + 1] == 'g' && ptr[st + 2] == 'z')
+    {
+        ptr[st] = '\0';
+        fileInfo->FilePathSize -= 3;
+        return true;
+    }
+
+    return false;
+}
+
+static bool DirectoryAddPath(FileInfo* dirInfo, const char* const path, const char* const fileName)
+{
+    if (FileInfoConstructor(dirInfo, path) != FILE_ERR_NO_ERRORS)
+    {
+        fprintf(stderr, "Ошибка создания FileInfo.\n");
+        return false;
+    }
+
+    if (FileConcatPath(dirInfo, fileName) != FILE_ERR_NO_ERRORS)
+    {
+        fprintf(stderr, "Ошибка конкатенации путей.\n");
+        return false;
+    }
+
+    return true;
+}
+
 static bool BackupCopy(FileInfo *src, FileInfo *dest)
 {
     assert(src);
@@ -172,49 +275,44 @@ static bool BackupCopy(FileInfo *src, FileInfo *dest)
     for (size_t srcIndex = 0; srcIndex < srcFilesCount; srcIndex++)
     {
         FileInfo *srcFile = (FileInfo *)ExtArrayGetElemAt(&src->Children, srcIndex);
-        size_t destIndex = ExtArrayFindElem(&dest->Children, srcFile, DirectoryComparator);
+        size_t destIndex = ExtArrayFindElem(&dest->Children, srcFile, DirectoryComparatorDest1);
 
         if (destIndex == -1)
         {
-            printf("Рекурсивное копирование \"%s\" в \"%s\".\n", srcFile->FilePath, dest->FilePath);
+            printf("Файл не найден. Рекурсивное копирование \"%s\" в \"%s\".\n", srcFile->FilePath, dest->FilePath);
             if (!BashCp(srcFile->FilePath, dest->FilePath))
             {
                 fprintf(stderr, "Ошибка копирования файлов.\n");
                 return false;
             }
 
-            // FileInfo destInfo = {};
-            // if (FileInfoConstructor(&destInfo, dest->FilePath) != FILE_ERR_NO_ERRORS)
-            // {
-            //     fprintf(stderr, "Ошибка создания FileInfo.\n");
-            //     return false;
-            // }
+            FileInfo destInfo = {};
+            if (!DirectoryAddPath(&destInfo, dest->FilePath, srcFile->FileName))
+            {
+                fprintf(stderr, "Ошибка инициализации.\n");
+                FileInfoDestructor(&destInfo);
+                return false;
+            }
 
-            // if (FileConcatPath(&destInfo, srcFile->FileName) != FILE_ERR_NO_ERRORS)
-            // {
-            //     fprintf(stderr, "Ошибка конкатенации путей.\n");
-            //     return false;
-            // }
+            if (!BashGzip(destInfo.FilePath))
+            {
+                fprintf(stderr, "Ошибка сжатия файлов.\n");
+                FileInfoDestructor(&destInfo);
+                return false;
+            }
 
-            // FileInfoDestructor(&destInfo);
+            FileInfoDestructor(&destInfo);
         }
         else
         {
             FileInfo *destFile = (FileInfo *)ExtArrayGetElemAt(&dest->Children, destIndex);
             if (srcFile->FileType == FILE_TYPE_DIR)
             {
-                // printf("Создание директории \"%s\".\n", destFile->FilePath);
-                // if (!BashMkdir(destFile->FilePath))
-                // {
-                //     fprintf(stderr, "Ошибка создания директории.\n");
-                //     return false;
-                // }
-
                 // Если нет изменений времени в папке, то это не значит, что файлы не менялись
                 //if (!CheckNeedCopy(srcFile, destFile))
                 //    continue;
 
-                printf("Умное копирование в директорию \"%s\".\n", destFile->FilePath);
+                printf("Анализ вложенной директории \"%s\".\n", destFile->FilePath);
                 if (!BackupCopy(srcFile, destFile))
                 {
                     fprintf(stderr, "Ошибка копирования файлов.\n");
@@ -224,15 +322,27 @@ static bool BackupCopy(FileInfo *src, FileInfo *dest)
             else
             {
                 if (!CheckNeedCopy(srcFile, destFile))
-                {
-                    //printf("Пропускаем копирование \"%s\" в \"%s\".\n", srcFile->FilePath, destFile->FilePath);
                     continue;
-                }
-                    
+                
                 printf("Копирование файла \"%s\" в \"%s\".\n", srcFile->FilePath, destFile->FilePath);
+                
+                if (!BashRm(destFile->FilePath))
+                {
+                    fprintf(stderr, "Ошибка удаления файла.\n");
+                    return false;
+                }
+
+                FileMaskGzip(destFile);
+
                 if (!BashCp(srcFile->FilePath, destFile->FilePath))
                 {
-                    fprintf(stderr, "Ошибка копирования файлов.\n");
+                    fprintf(stderr, "Ошибка копирования файла.\n");
+                    return false;
+                }
+
+                if (!BashGzip(destFile->FilePath))
+                {
+                    fprintf(stderr, "Ошибка удаления файла.\n");
                     return false;
                 }
             }
